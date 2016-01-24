f77733a3a8cc5d80f8b9dd3bef166447f19bf113
Charles Leifer <coleifer@gmail.com>
2016-01-23 21:57:57 -0600

    Fleshing out implementation.

--- forestdb.pyx
+++ forestdb.pyx
@@ -214,8 +214,8 @@ cdef extern from "libforestdb/forestdb.h":
 
     cdef size_t fdb_estimate_space_used(fdb_file_handle *handle)
     cdef fdb_status fdb_get_file_info(fdb_file_handle *h, fdb_file_info *i)
-    cdef fdb_status fdb_get_kvs_info(fdb_file_handle *h, fdb_kvs_info *i)
-    cdef fdb_status fdb_get_kvs_ops_info(fdb_file_handle *, fdb_kvs_ops_info *)
+    cdef fdb_status fdb_get_kvs_info(fdb_kvs_handle *h, fdb_kvs_info *i)
+    cdef fdb_status fdb_get_kvs_ops_info(fdb_kvs_handle *, fdb_kvs_ops_info *)
 
     cdef fdb_status fdb_get_kvs_seqnum(fdb_kvs_handle *, uint64_t *)
     cdef fdb_status fdb_get_kvs_name_list(fdb_file_handle *,
@@ -238,7 +238,9 @@ cdef extern from "libforestdb/forestdb.h":
                                          fdb_kvs_config **)
 
     cdef fdb_status fdb_kvs_close(fdb_kvs_handle *)
+    cdef fdb_status fdb_kvs_remove(fdb_file_handle *, const char *)
     cdef const char *fdb_error_msg(int err)
+    cdef size_t fdb_get_buffer_cache_used()
 
 
 cdef uint32_t FDB_OPEN_FLAG_CREATE = 1
@@ -290,8 +292,20 @@ cdef int FDB_RESULT_SEEK_FAIL = -13
 cdef bytes encode(obj):
     if isinstance(obj, unicode):
         return obj.encode('utf-8')
+    elif obj is None:
+        return obj
     return bytes(obj)
 
+cdef decode(char *ptr, size_t n, free_ptr=True):
+    value = ptr[:n]
+    if free_ptr:
+        free(ptr)
+    if IS_PY3K:
+        try:
+            return value.decode('utf-8')
+        except UnicodeDecodeError:
+            pass
+    return value
 
 cdef bint IS_PY3K = sys.version_info[0] == 3
 
@@ -325,7 +339,7 @@ cdef class ForestDB(object):
         if isinstance(filename, unicode):
             self.encoded_filename = fsencode(filename)
         else:
-            self.encoded_filename = bytes(filename)
+            self.encoded_filename = encode(filename)
 
         # Update configuration values.
         self.config = fdb_get_default_config()
@@ -350,6 +364,36 @@ cdef class ForestDB(object):
             self.handle = <fdb_file_handle *>0
         self.is_open = False
         return True
+
+    def kv(self, name, **config):
+        return KVStore(self, name, **config)
+
+    def commit(self):
+        _errcheck(fdb_commit(self.handle, FDB_COMMIT_NORMAL))
+        return True
+
+    def flush_wal(self):
+        _errcheck(fdb_commit(self.handle, FDB_COMMIT_MANUAL_WAL_FLUSH))
+        return True
+
+    def buffer_cache_used(self):
+        cdef size_t cache_used = fdb_get_buffer_cache_used()
+        return cache_used
+
+    def info(self):
+        cdef:
+            fdb_file_info info
+
+        _errcheck(fdb_get_file_info(self.handle, &info))
+
+        return {
+            'filename': info.filename,
+            'new_filename': info.new_filename,
+            'doc_count': info.doc_count,
+            'deleted_count': info.deleted_count,
+            'space_used': info.space_used,
+            'file_size': info.file_size,
+            'num_kv_stores': info.num_kv_stores}
 
 
 cdef class KVStore(object):
@@ -399,22 +443,30 @@ cdef class KVStore(object):
         self.is_open = False
         return True
 
-    cpdef bint set(self, key, value):
+    cpdef drop(self):
+        self.close()
+        fdb_kvs_remove(self.db.handle, self.name)
+
+    cpdef bint set(self, key, body):
         cdef:
             char *kptr
-            char *vptr
+            char *bptr
             int rc
-            Py_ssize_t klen, vlen
+            Py_ssize_t klen, blen
 
         if IS_PY3K:
             key = encode(key)
-            value = encode(value)
+            body = encode(body)
 
         PyBytes_AsStringAndSize(key, &kptr, &klen)
-        PyBytes_AsStringAndSize(value, &vptr, &vlen)
+        PyBytes_AsStringAndSize(body, &bptr, &blen)
 
-        rc = fdb_set_kv(self.handle, <void *>kptr, <size_t>klen, <void *>vptr, <size_t>vlen)
-        _errcheck(rc)
+        _errcheck(fdb_set_kv(
+            self.handle,
+            <void *>kptr,
+            <size_t>klen,
+            <void *>bptr,
+            <size_t>blen))
 
         return True
 
@@ -433,9 +485,9 @@ cdef class KVStore(object):
     cpdef get(self, key):
         cdef:
             char *kptr
-            char *vptr
+            char *bptr
             int rc
-            Py_ssize_t klen, vlen
+            Py_ssize_t klen, blen
 
         if IS_PY3K:
             key = encode(key)
@@ -445,14 +497,271 @@ cdef class KVStore(object):
             self.handle,
             <void *>kptr,
             <size_t>klen,
-            <void **>(&vptr),
-            <size_t *>(&vlen))
+            <void **>(&bptr),
+            <size_t *>(&blen))
         if rc == 0:
-            value = vptr[:vlen]
-            free(vptr)
-            if IS_PY3K:
-                try:
-                    return value.decode('utf-8')
-                except UnicodeDecodeError:
-                    pass
-            return value
+            return decode(bptr, blen)
+
+    cpdef get_by_seqnum(self, seqnum):
+        cdef:
+            Document document
+            fdb_doc *doc
+
+        _errcheck(fdb_doc_create(&doc, NULL, 0, NULL, 0, NULL, 0))
+        doc.seqnum = seqnum
+        _errcheck(fdb_get_byseq(self.handle, doc))
+        document = Document.__new__(Document, self, _create=False)
+        document.set_document(doc)
+        return document
+
+    def __setitem__(self, key, body):
+        self.set(key, body)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            pass
+        elif isinstance(key, int):
+            pass
+        else:
+            return self.get(key)
+
+    def __delitem__(self, key):
+        if isinstance(key, slice):
+            pass
+        else:
+            self.delete(key)
+
+    def document(self, key, meta=None, body=None):
+        return Document.__new__(Document, self, key, meta, body)
+
+    cpdef last_seqnum(self):
+        cdef uint64_t seqnum
+        _errcheck(fdb_get_kvs_seqnum(self.handle, &seqnum))
+        return seqnum
+
+    def info(self):
+        cdef:
+            fdb_kvs_info info
+
+        _errcheck(fdb_get_kvs_info(self.handle, &info))
+        return {
+            'name': info.name,
+            'last_seqnum': info.last_seqnum,
+            'doc_count': info.doc_count,
+            'deleted_count': info.deleted_count,
+            'space_used': info.space_used}
+
+    def ops_info(self):
+        cdef:
+            fdb_kvs_ops_info info
+
+        _errcheck(fdb_get_kvs_ops_info(self.handle, &info))
+        return {
+            'num_sets': info.num_sets,
+            'num_dels': info.num_dels,
+            'num_commits': info.num_commits,
+            'num_compacts': info.num_compacts,
+            'num_gets': info.num_gets,
+            'num_iterator_gets': info.num_iterator_gets,
+            'num_iterator_moves': info.num_iterator_moves}
+
+
+cdef class Document(object):
+    cdef:
+        fdb_doc *handle
+        readonly KVStore kv
+
+    def __cinit__(self, KVStore kv, key=None, meta=None, body=None,
+                  _create=True):
+        cdef:
+            bytes bkey, bmeta, bbody
+            char *_key
+            char *_meta = NULL
+            char *_body = NULL
+            Py_ssize_t _key_len, _meta_len = 0, _body_len = 0
+            fdb_status status
+
+        self.kv = kv
+
+        if _create:
+            bkey = encode(key)
+            bmeta = encode(meta)
+            bbody = encode(body)
+
+            if key is not None:
+                PyBytes_AsStringAndSize(bkey, &_key, &_key_len)
+            if meta is not None:
+                PyBytes_AsStringAndSize(bmeta, &_meta, &_meta_len)
+            if body is not None:
+                PyBytes_AsStringAndSize(bbody, &_body, &_body_len)
+
+            _errcheck(fdb_doc_create(
+                &self.handle,
+                <void *>(_key),
+                <size_t>(_key_len),
+                <void *>(_meta),
+                <size_t>(_meta_len),
+                <void *>(_body),
+                <size_t>(_body_len)))
+
+    def __dealloc__(self):
+        if self.handle:
+            fdb_doc_free(self.handle)
+
+    cdef set_document(self, fdb_doc *doc):
+        self.handle = doc
+
+    def update(self, meta, body):
+        cdef:
+            char *_meta = NULL
+            char *_body = NULL
+            Py_ssize_t _meta_len = 0, _body_len = 0
+
+        if IS_PY3K:
+            meta = encode(meta)
+            body = encode(body)
+
+        if meta is not None:
+            PyBytes_AsStringAndSize(meta, &_meta, &_meta_len)
+        if body is not None:
+            PyBytes_AsStringAndSize(body, &_body, &_body_len)
+
+        _errcheck(fdb_doc_update(
+            &(self.handle),
+            <void *>(_meta),
+            <size_t>(_meta_len),
+            <void *>(_body),
+            <size_t>(_body_len)))
+
+    def insert(self):
+        _errcheck(fdb_set(self.kv.handle, self.handle))
+        return self.handle.seqnum
+
+    def get(self):
+        _errcheck(fdb_get(self.kv.handle, self.handle))
+        return self.handle.seqnum
+
+    def get_metadata(self):
+        _errcheck(fdb_get_metaonly(self.kv.handle, self.handle))
+        return self.meta
+
+    def delete(self):
+        _errcheck(fdb_del(self.kv.handle, self.handle))
+
+    property key:
+        def __get__(self):
+            return decode(<char *>self.handle.key, self.handle.keylen, False)
+
+    property meta:
+        def __get__(self):
+            return decode(<char *>self.handle.meta, self.handle.metalen, False)
+
+        def __set__(self, value):
+            self.update(value, self.body)
+
+    property body:
+        def __get__(self):
+            return decode(<char *>self.handle.body, self.handle.bodylen, False)
+
+        def __set__(self, value):
+            self.update(self.meta, value)
+
+    property seqnum:
+        def __get__(self):
+            return self.handle.seqnum
+
+    property disk_offset:
+        def __get__(self):
+            return self.handle.offset
+
+
+cdef class Cursor(object):
+    cdef:
+        bint reverse
+        bint stopped
+        KVStore kv
+        fdb_iterator *handle
+
+    def __cinit__(self, KVStore kv, start=None, stop=None, skip_start=False,
+                  skip_stop=False, reverse=False):
+        cdef:
+            bytes bstart, bstop
+            char *_start = NULL
+            char *_stop = NULL
+            Py_ssize_t start_len = 0, stop_len = 0
+            uint16_t options
+
+        bstart = encode(start)
+        bstop = encode(stop)
+
+        if start is not None:
+            PyBytes_AsStringAndSize(bstart, &_start, &start_len)
+        if stop is not None:
+            PyBytes_AsStringAndSize(bstop, &_stop, &stop_len)
+
+        self.kv = kv
+        self.reverse = reverse
+
+        options = FDB_ITR_NO_DELETES
+        if skip_start:
+            options |= FDB_ITR_SKIP_MIN_KEY
+        if skip_stop:
+            options |= FDB_ITR_SKIP_MAX_KEY
+
+        _errcheck(fdb_iterator_init(
+            self.kv.handle,
+            &self.handle,
+            _start,
+            start_len,
+            _stop,
+            stop_len,
+            options))
+        self.stopped = False
+
+        #cdef uint8_t FDB_ITR_SEEK_HIGHER = 0x00
+        #cdef uint8_t FDB_ITR_SEEK_LOWER = 0x01
+
+    def __dealloc__(self):
+        if self.handle:
+            fdb_iterator_close(self.handle)
+
+    def __next__(self):
+        cdef:
+            Document document
+            fdb_doc *doc
+            fdb_status status
+
+        if self.stopped:
+            raise StopIteration
+
+        status = fdb_iterator_get(self.handle, &doc)
+        if status != FDB_RESULT_SUCCESS:
+            raise StopIteration
+
+        document = Document.__new__(Document, _create=False)
+        document.set_document(doc)
+
+        if self.reverse:
+            status = fdb_iterator_prev(self.handle)
+        else:
+            status = fdb_iterator_next(self.handle)
+
+        if status == FDB_RESULT_ITERATOR_FAIL:
+            self.stopped = True
+
+        return document
+
+    def seek(self, key, mode=FDB_ITR_SEEK_HIGHER):
+        cdef:
+            bytes bkey = encode(key)
+            char *_key
+            Py_ssize_t keylen
+
+        PyBytes_AsStringAndSize(bkey, &_key, &keylen)
+        _errcheck(fdb_iterator_seek(self.handle, _key, keylen, mode))
+
+    def first(self):
+        _errcheck(fdb_iterator_seek_to_min(self.handle))
+
+    def last(self):
+        _errcheck(fdb_iterator_seek_to_max(self.handle))
